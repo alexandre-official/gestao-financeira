5d3607a0b5ce5c6376078b2c316c547f84ec4b0b
alexandre-official <alexandrinho22112004@icloud.com>
2025-07-05 18:57:14 -0300

    0.12

---
+++ interface/interface.js
@@ -0,0 +1,6 @@
+function addErro() {
+    const bloco_de_erro = window.document.createElement('div')
+    bloco_de_erro.classList.add('bloco-de-erro')
+    window.document.appendChild(bloco_de_erro)
+}
+addErro()
