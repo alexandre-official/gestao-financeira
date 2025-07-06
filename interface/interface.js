801f021cf18d6d4cd3ed01ec901e3ac54c1e1d05
alexandre-official <alexandrinho22112004@icloud.com>
2025-07-06 00:14:17 -0300

    0.13

--- interface/interface.js
+++ interface/interface.js
@@ -1,6 +1,23 @@
-function addErro() {
+function addErro(t) {
     const bloco_de_erro = window.document.createElement('div')
     bloco_de_erro.classList.add('bloco-de-erro')
-    window.document.appendChild(bloco_de_erro)
+    window.document.body.appendChild(bloco_de_erro)
+    //h1
+    const h1 = criarH1('Erro')
+    bloco_de_erro.appendChild(h1)
+    //p
+    const p = criarP(t)
+    bloco_de_erro.appendChild(p)
 }
-addErro()
+function criarH1(s) {
+    const h1 = window.document.createElement('h1')
+    h1.innerHTML = s
+    return h1
+}
+function criarP(s) {
+    const p = window.document.createElement('p')
+    p.innerHTML = s
+    return p
+}
+addErro('Adicione um valor!')
+addErro("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam eum consectetur deleniti soluta dolores, ipsam repellat accusamus. Ratione officia tempore voluptate quasi fugit eos cupiditate reprehenderit! Exercitationem saepe neque laboreLorem ipsum dolor sit amet consectetur adipisicing elit Numquam eum consectetur deleniti soluta dolores ipsam repellat accusamus. Ratione officia tempore voluptate quasi fugit eos cupiditate reprehenderit Exercitationem saepe neque laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eum consectetur deleniti soluta dolores, ipsam repellat accusamus. Ratione officia tempore voluptate quasi fugit eos cupiditate reprehenderit! Exercitationem saepe neque laboreorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam eum consectetur deleniti soluta dolores, ipsam repellat accusamus. Ratione officia tempore voluptate quasi fugit eos cupiditate reprehenderit! Exercitationem saepe neque labore.orem, ipsum dolor sit amet consectetur adipisicing elit. Numquam eum consectetur deleniti soluta dolores, ipsam repellat accusamus. Ratione officia tempore voluptate quasi fugit eos cupiditate reprehenderit! Exercitationem saepe neque labore")
