45f7a00357c5ce90996a13e480fce5c4f03bf427
alexandre-official <alexandrinho22112004@icloud.com>
2025-07-21 15:01:21 -0300

    0.62

--- calculos-de/acoes/acoes.js
+++ calculos-de/acoes/acoes.js
@@ -7,9 +7,12 @@ function processar() {
     let select_aporte = String(window.document.querySelector('#select-aporte-id').value)
     let aporte = Number(window.document.querySelector('#aporte-id').value)
 
+    let select_juros = String(window.document.querySelector('#select-juros-id').value)
     let juros = Number(window.document.querySelector('#juros-id').value)
 
+    let select_tempo_investido = String(window.document.querySelector('#select-tempo-investido-id').value)
     let tempo_investido = Number(window.document.querySelector('#tempo-investido-id').value)
+    
     let patrimonio = 0
     let patrimonio_investido = 0
 
@@ -61,49 +64,71 @@ function processar() {
         addResposta()
     }
     function calcular() {
-        for(let i = 0; tempo_investido > i; i++) {
-            patrimonio = patrimonio + aporte
+        function verificarSelects() {
+            if(select_aporte === 'anual') {
+                aporte = (aporte / 12)
+            }
+            if(select_juros === 'anual') {
+                juros = (juros / 12)
+            }
+            if(select_tempo_investido === 'anos') {
+                tempo_investido = tempo_investido * 12
+            }
         }
-        p.innerHTML = patrimonio
+        verificarSelects()
+        function patrimonioSemInvestir() {
+            patrimonio = investido_inicial
+            for(let i = 0; i < tempo_investido; i++) {
+                patrimonio = patrimonio + aporte
+            }
+        }
+        patrimonioSemInvestir()
+        function aplicarJurosSobreJuros() {
+            patrimonio_investido = investido_inicial
+            for(let i = 0; tempo_investido > i; i++) {
+                patrimonio_investido = patrimonio_investido + aporte
+                
+                patrimonio_investido = patrimonio_investido + (juros * (patrimonio_investido / 100))
+            }
+        }
+        aplicarJurosSobreJuros()
     }
     function addResposta() {
         addBlocoDeResposta()
+        addH1('Investimento')
         if(investido_inicial) {
             addDiv(
-                addH1('Investido inicial', 'divCall'),
-                addH2(virarMoeda(investido_inicial), 'divCall')
+                addH2('Investido inicial', 'divCall'),
+                addP(virarMoeda(investido_inicial), 'divCall')
             )
         }
         if(aporte) {
             addDiv(
-                addH1(`Aporte ${select_aporte}`, 'divCall'),
-                addH2(virarMoeda(aporte), 'divCall')
+                addH2(`Aporte ${select_aporte}`, 'divCall'),
+                addP(virarMoeda(aporte), 'divCall')
             )
         }
         addDiv(
-            addH1('Juros por ano', 'divCall'),
-            addH2(`${juros}%`, 'divCall')
+            addH2(`Juros ${select_juros}`, 'divCall'),
+            addP(`${juros}%`, 'divCall')
         )
         addDiv(
-            addH1('Tempo investido', 'divCall'),
-            addH2(`${tempo_investido} anos`, 'divCall')
+            addH2(`Tempo investido em ${select_tempo_investido}`, 'divCall'),
+            addP(`${tempo_investido} ${select_tempo_investido}`, 'divCall')
         )
+        addH1('Rendimento')
+        calcular()
         addDiv(
-            addH1('Patrimonio sem investir', 'divCall'),
+            addH2('Patrimonio sem investir', 'divCall'),
             addP(`${virarMoeda(patrimonio)}`,'divCall')
         )
         addDiv(
-            addH1('Patrimonio investido', 'divCall'),
+            addH2('Patrimonio investido', 'divCall'),
             addP(virarMoeda(patrimonio_investido),'divCall')
         )
         addDiv(
-            addH1('Diferença de', 'divCall'),
+            addH2('Diferença de', 'divCall'),
             addP(`${virarMoeda(patrimonio_investido - patrimonio)}`, 'divCall')
         )
-        /*
-        addDiv(
-            addH1(,'divCall'),
-            addP(,'divCall')
-        )*/
     }
 }
